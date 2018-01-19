140dcaa7c0b3393dd6620baf7131981cccf61672
zig <zig1375@gmail.com>
2018-01-19 16:33:48 +0300

    swift4

--- Package.swift
+++ Package.swift
@@ -1,28 +1,8 @@
-// swift-tools-version:3.1
-//
-//  Package.swift
-//  Lua
-//
-
 import PackageDescription
 
 let package = Package(
 	name: "Lua",
 	dependencies: [
-		.Package(url: "https://github.com/DavidSkrundz/CLua.git", majorVersion: 5, minor: 2),
+		.Package(url: "https://github.com/Zig1375/CLua.git", majorVersion: 0)
 	]
 )
-
-let staticLibrary = Product(
-	name: "Lua",
-	type: .Library(.Static),
-	modules: ["Lua"]
-)
-let dynamicLibrary = Product(
-	name: "Lua",
-	type: .Library(.Dynamic),
-	modules: ["Lua"]
-)
-
-products.append(staticLibrary)
-products.append(dynamicLibrary)
