a979b9ad6155ca9fdb450ee9ac6fba69eccb8ddd
zig <zig1375@gmail.com>
2018-01-19 16:39:07 +0300

    swift4

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "Lua",
 	dependencies: [
-		.Package(url: "https://github.com/Zig1375/CLua.git", majorVersion: 0)
+		.Package(url: "https://github.com/Zig1375/CLua.git", majorVersion: 0, minor: 1)
 	]
 )
